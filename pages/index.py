5ec8ecd362011598f7d717e10368f2f1fe0b70df
JeanFraga <fragajean7@gmail.com>
2019-10-24 22:20:00 -0400

    another try

--- pages/index.py
+++ pages/index.py
@@ -4,33 +4,33 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-# import pandas as pd
-# from joblib import load
-# from xgboost import XGBClassifier
-# import category_encoders as ce
-# from sklearn.pipeline import make_pipeline
-# from sklearn.model_selection import GridSearchCV, StratifiedKFold
+import pandas as pd
+from joblib import load
+from xgboost import XGBClassifier
+import category_encoders as ce
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 
 from app import app
 
 # url = 'https://raw.githubusercontent.com/JeanFraga/DS8-Build_Week-1/master/notebooks/Restaurant_Consumer_Data_merged'
 
-# df = pd.read_csv(url)
-# pipeline1 = load('assets/xgboost_model_y1.joblib')
-# pipeline2 = load('assets/xgboost_model_y2.joblib')
-# pipeline3 = load('assets/xgboost_model_y3.joblib')
-# pipelines = {}
-# pipelines['pipeline1'] = pipeline1
-# pipelines['pipeline2'] = pipeline2
-# pipelines['pipeline3'] = pipeline3
-# target1 = 'rating'
-# target2 = 'food_rating'
-# target3 = 'service_rating'
+df = pd.read_csv(url)
+pipeline1 = load('assets/xgboost_model_y1.joblib')
+pipeline2 = load('assets/xgboost_model_y2.joblib')
+pipeline3 = load('assets/xgboost_model_y3.joblib')
+pipelines = {}
+pipelines['pipeline1'] = pipeline1
+pipelines['pipeline2'] = pipeline2
+pipelines['pipeline3'] = pipeline3
+target1 = 'rating'
+target2 = 'food_rating'
+target3 = 'service_rating'
 
-# X = df.drop(columns=[target1, target2, target3])
-# y1 = df[target1]
-# y2 = df[target2]
-# y3 = df[target3]
+X = df.drop(columns=[target1, target2, target3])
+y1 = df[target1]
+y2 = df[target2]
+y3 = df[target3]
 
 
 """
@@ -79,17 +79,17 @@ column1 = dbc.Col(
 )
 
 
-# importances = pd.Series(pipeline1.best_estimator_.named_steps['xgbclassifier'].feature_importances_, X.columns)
-# n=25
-# importances = importances.sort_values()[-n:]
-# importances = importances.to_frame().reset_index()
-# importances.columns=['column1','column2']
+importances = pd.Series(pipeline1.best_estimator_.named_steps['xgbclassifier'].feature_importances_, X.columns)
+n=25
+importances = importances.sort_values()[-n:]
+importances = importances.to_frame().reset_index()
+importances.columns=['column1','column2']
 
-# fig = px.bar(importances,y='column1',x='column2',title=f'Top {n} features',  orientation='h',width=700, height=700)
+fig = px.bar(importances,y='column1',x='column2',title=f'Top {n} features',  orientation='h',width=700, height=700)
 
-gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-           hover_name="country", log_x=True, size_max=80)
+# gapminder = px.data.gapminder()
+# fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
+#            hover_name="country", log_x=True, size_max=80)
 
 column2 = dbc.Col(
     [
