3695a46518dc6ee4eda88f6dd406b3f799f6e463
JeanFraga <fragajean7@gmail.com>
2019-10-24 22:54:11 -0400

    added images

--- pages/insights.py
+++ pages/insights.py
@@ -13,6 +13,30 @@ column1 = dbc.Col(
         
             ## Insights
 
+            The dataset I worked with came separated into 7 different CSV files that contained the following.
+            
+            Restaurants
+            R1 = 1 chefmozaccepts.csv
+
+            R2 = 2 chefmozcuisine.csv
+
+            R3 = 3 chefmozhours4.csv
+
+            R4 = 4 chefmozparking.csv
+
+            R5 = 5 geoplaces2.csv
+
+            User
+            U6 = 6 usercuisine.csv
+
+            U7 = 7 userpayment.csv
+
+            U8 = 8 userprofile.csv
+
+            User-Item-Rating
+            U9 = 9 rating_final.csv
+
+            👉 please take a look at some interesting interactions I found before I even started to model the data.
 
             """
         ),
@@ -23,7 +47,7 @@ column1 = dbc.Col(
 
 column2 = dbc.Col(
     [
-        
+        html.Img(src=app.get_asset_url('restaurant_payments.png'), className='img-fluid')
     ]
 )
 
